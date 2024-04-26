0488a34771c7667ec6b0c2d8edaee845afc17a7a
Sriram Velamur <sriram.velamur@gmail.com>
2024-04-26 16:49:42 +0530

    Cleans up loggers and error conditions for email helper.

--- helpers/communications/email.js
+++ helpers/communications/email.js
@@ -6,13 +6,12 @@ let emailer
 export const communicationChannels = {
   email: {
     send(mailOptions) {
-      emailer.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.error("Error sending email: ", error);
-        } else {
-          logger.log("Email sent: ", info);
-        }
-      });
+      emailer.sendMail(mailOptions, (error, info = {}) => {
+        const { accepted, messageId } = info
+        if (!accepted?.length) return logger.error(`sendMail`, `No recipients could be mailed.`)
+        if (error) return logger.error(`sendMail`, error)
+        logger.log(`sendMail`, {accepted, messageId })
+      })
     }
   }
 }
