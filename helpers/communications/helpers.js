4ef57f2967e55c516ee401dbd1b918d61a297d88
Sriram Velamur <sriram.velamur@gmail.com>
2024-05-01 23:07:07 +0530

    Updates renderTemplate API to accept either a templatePath or raw contents.

--- helpers/communications/helpers.js
+++ helpers/communications/helpers.js
@@ -1,8 +1,9 @@
 import { readFile } from 'fs/promises';
 import { resolve } from 'path';
 
-export async function renderTemplate(templatePath, data) {
-  const template = (await readFile(resolve(templatePath))).toString()
+export async function renderTemplate({ templatePath, data, contents }) {
+  if (!contents && !templatePath) throw new Error(`Either a templatePath or loaded contents is needed for renderer.`)
+  const template = contents ? contents : (await readFile(resolve(templatePath))).toString()
   const regex = /\{\{(.*?)\}\}/g;
   return template.replace(regex, (_, key) => {
     return data.hasOwnProperty(key) ? data[key] : "";
